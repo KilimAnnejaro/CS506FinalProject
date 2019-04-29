900cef54942b39c67c4ce1234fb326a21eded963
Brianna Benson <briannabenson@Briannas-MacBook-Pro.local>
2019-04-28 22:42:19 -0500

    candidate information added

--- AwesomeProject/screens/RankingScreen.js
+++ AwesomeProject/screens/RankingScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text, Image } from 'react-native';
+import { ScrollView, StyleSheet, Text, Image, TouchableOpacity, Alert } from 'react-native';
 import { Button } from 'native-base';
 import * as firebase from "firebase";
 
@@ -17,13 +17,12 @@ export default class RankingScreen extends React.Component {
       candidates: ["Cory Booker", "Donald Trump", "Ted Cruz", "Kamala Harris", "Barack Obama"],
       currentParty: 'Democratic',
       parties: ["Democratic", "Republican", "Republican", "Democratic", "Democratic"],
-      currentCandidateImage: '../assets/images/img_booker.jpg',
-      candidateImages: [
-        '../assets/images/img_booker.jpg',
-        '../assets/images/img_trump.jpg',
-        '../assets/images/img_cruz.jpg',
-        '../assets/images/img_harris.jpg',
-        '../assets/images/img_obama.jpg',
+      info: [
+        "Booker is a United States Senator from New Jersey. He was first elected in 2013 as the first African American Sentor from New Jersey. Booker supports women's rights, affirmative action, same-sex marriage and single-payer healthcare.",
+        "Trump is the 45th President of the United States. Before he was president, Trump was in charge of his family's business, The Trump Organization. He is most know for his America First agenda in foreign policy and the tax cut package he has enabled for individuals and businesses.",
+        "Cruz is a United States Senator from Texas. Cruz was first elected in 2013, and in 2016, he was the runner-up for the Republican nomination for the presidential election. He is in favor of the death penalty, the USA Freedom Act, school choice, and gun rights.",
+        "Harris is a United States Senator from California. She was first elected in 2017. Harris has supported Medicare-for-all, legalization of recreational marijuana, sanctuary cities, passing a DREAM Act, and lowering taxes for the working and middle classes.",
+        "Obama was the 44th President of the United States. He was previously the senator of Illinios. Obama is known for passing the Patient Protection and Affordable Care Act (Obamacare). He also repealed Don't Ask, Don't Tell and reduced nuclear weapons."
       ]
     })
   }
@@ -42,8 +41,14 @@ export default class RankingScreen extends React.Component {
       currentIndex: (this.state.currentIndex + 1)%5,
       currentCandidate: this.state.candidates[newVal],
       currentParty: this.state.parties[newVal],
-      currentCandidateImage: this.state.candidateImages[newVal]
     })
+  }
+
+  candidateInfo = () => {
+    Alert.alert(
+      this.state.currentCandidate,
+      "Party:" + this.state.currentParty + "\n\n" + this.state.info[this.state.currentIndex],
+    )
   }
 
   render() {
@@ -52,7 +57,9 @@ export default class RankingScreen extends React.Component {
         <ScrollView style={styles.container}>
         <Text style={styles.text}>Candidate: {this.state.currentCandidate}</Text>
         <Text style={styles.textSmall}>Party: {this.state.currentParty}</Text>
-        <Image source={require('../assets/images/img_booker.jpg')} style={styles.image}/>
+        <TouchableOpacity onPress={()=>this.candidateInfo()}>
+          <Image source={require('../assets/images/img_booker.jpg')} style={styles.image}/>
+        </TouchableOpacity>
         <Button style={{marginTop: 20, marginLeft: 25, marginRight: 25}}
               full
               rounded
@@ -104,7 +111,9 @@ export default class RankingScreen extends React.Component {
         <ScrollView style={styles.container}>
         <Text style={styles.text}>Candidate: {this.state.currentCandidate}</Text>
         <Text style={styles.textSmall}>Party: {this.state.currentParty}</Text>
-        <Image source={require('../assets/images/img_trump.jpg')} style={styles.image}/>
+        <TouchableOpacity onPress={()=>this.candidateInfo()}>
+          <Image source={require('../assets/images/img_trump.jpg')} style={styles.image}/>
+        </TouchableOpacity>
         <Button style={{marginTop: 20, marginLeft: 25, marginRight: 25}}
               full
               rounded
@@ -156,7 +165,9 @@ export default class RankingScreen extends React.Component {
         <ScrollView style={styles.container}>
         <Text style={styles.text}>Candidate: {this.state.currentCandidate}</Text>
         <Text style={styles.textSmall}>Party: {this.state.currentParty}</Text>
-        <Image source={require('../assets/images/img_cruz.jpg')} style={styles.image}/>
+        <TouchableOpacity onPress={()=>this.candidateInfo()}>
+          <Image source={require('../assets/images/img_cruz.jpg')} style={styles.image}/>
+        </TouchableOpacity>
         <Button style={{marginTop: 20, marginLeft: 25, marginRight: 25}}
               full
               rounded
@@ -208,7 +219,9 @@ export default class RankingScreen extends React.Component {
         <ScrollView style={styles.container}>
         <Text style={styles.text}>Candidate: {this.state.currentCandidate}</Text>
         <Text style={styles.textSmall}>Party: {this.state.currentParty}</Text>
-        <Image source={require('../assets/images/img_harris.jpg')} style={styles.image}/>
+        <TouchableOpacity onPress={()=>this.candidateInfo()}>
+          <Image source={require('../assets/images/img_harris.jpg')} style={styles.image}/>
+        </TouchableOpacity>
         <Button style={{marginTop: 20, marginLeft: 25, marginRight: 25}}
               full
               rounded
@@ -260,7 +273,9 @@ export default class RankingScreen extends React.Component {
         <ScrollView style={styles.container}>
         <Text style={styles.text}>Candidate: {this.state.currentCandidate}</Text>
         <Text style={styles.textSmall}>Party: {this.state.currentParty}</Text>
-        <Image source={require('../assets/images/img_obama.jpg')} style={styles.image}/>
+        <TouchableOpacity onPress={()=>this.candidateInfo()}>
+          <Image source={require('../assets/images/img_obama.jpg')} style={styles.image}/>
+        </TouchableOpacity>
         <Button style={{marginTop: 20, marginLeft: 25, marginRight: 25}}
               full
               rounded
